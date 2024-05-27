f0b42b980f396e21878304cbb36e0f8c04649345
Kunal Garg <kunalgarg925@gmail.com>
2024-05-27 20:52:18 +0530

    allow all origin change

--- src/main/java/com/srvme/broadcastingsystem/config/WebSocketConfig.java
+++ src/main/java/com/srvme/broadcastingsystem/config/WebSocketConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new MyWebSocketHandler(), "/ws");
+        registry.addHandler(new MyWebSocketHandler(), "/ws")
+                .setAllowedOrigins("*"); // Allow all origins;
     }
 }
